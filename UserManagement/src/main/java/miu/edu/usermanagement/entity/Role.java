814436a3304f3fbd74186449731a1e42e9649c14
thaihainguyen <thainguyen@miu.edu>
2020-10-23 15:58:26 -0500

    Create Role.java

---
+++ UserManagement/src/main/java/miu/edu/usermanagement/entity/Role.java
@@ -0,0 +1,28 @@
+package miu.edu.usermanagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role extends BaseModel{
+
+    @Column(unique = true)
+    private String name;
+    private String description;
+    private int lv;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "role_privilege", joinColumns = {
+            @JoinColumn(name = "role_id")}, inverseJoinColumns = {
+            @JoinColumn(name = "privilege_id")})
+    private List<Privilege> privileges;
+}
