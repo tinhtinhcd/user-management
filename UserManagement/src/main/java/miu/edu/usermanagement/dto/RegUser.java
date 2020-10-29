08406544d04fc9a6a79f55c62686897f5fe2d21a
tinhtinhcd <tinhtinhcd@gmail.com>
2020-10-29 18:35:46 -0500

    implement euruka

--- UserManagement/src/main/java/miu/edu/usermanagement/dto/RegUser.java
+++ UserManagement/src/main/java/miu/edu/usermanagement/dto/RegUser.java
@@ -2,7 +2,10 @@ package miu.edu.usermanagement.dto;
 
 import lombok.*;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
--- UserManagement/src/main/resources/import.sql
+++ UserManagement/src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO `role` VALUES (1,'2020-10-27 17:21:42','Administration',1,'admin');
-INSERT INTO `role` VALUES (2,'2020-10-27 17:21:42','Vendor',1,'vendor');
-INSERT INTO `role` VALUES (3,'2020-10-27 17:21:42','Client',1,'client');
-INSERT INTO `role` VALUES (4,'2020-10-27 17:21:42','Consumer',1,'user');
-INSERT INTO `user` VALUES (1,'2020-10-23 08:31:42','admin@miu.edu','Administrator','Miu','admin','(800)369-6480',_binary '','admin');
-INSERT INTO `user_role` VALUES (1,1);
+# INSERT INTO `role` VALUES (1,'2020-10-27 17:21:42','Administration',1,'admin');
+# INSERT INTO `role` VALUES (2,'2020-10-27 17:21:42','Vendor',1,'vendor');
+# INSERT INTO `role` VALUES (3,'2020-10-27 17:21:42','Client',1,'client');
+# INSERT INTO `role` VALUES (4,'2020-10-27 17:21:42','Consumer',1,'user');
+# INSERT INTO `user` VALUES (1,'2020-10-23 08:31:42','admin@miu.edu','Administrator','Miu','admin','(800)369-6480',_binary '','admin');
+# INSERT INTO `user_role` VALUES (1,1);
