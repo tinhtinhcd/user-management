9683413337c6f44e4a060405c8dc4ba4be909ee1
thaihainguyen <thainguyen@miu.edu>
2020-10-30 13:59:21 -0500

    Exceptions

---
+++ UserManagement/src/main/java/miu/edu/usermanagement/exception/UsernameExistedException.java
@@ -0,0 +1,20 @@
+package miu.edu.usermanagement.exception;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.MessageSourceResourceBundle;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Locale;
+
+public class UsernameExistedException extends RuntimeException {// Throwable {
+
+//    @Autowired
+//    private MessageSource messageSrc;
+
+    public UsernameExistedException(@NotNull @Size(max = 10, message = "{error.username.length}") String username) {
+        //super(messageSrc.getMessage("user.new.fail", new String[]{username}, Locale.US));
+        super(String.format("Advice: The username %s existed", username));
+    }
+}
