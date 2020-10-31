21865b460fe3d8e0f01e3fe1429ac92ed887022b
thaihainguyen <thainguyen@miu.edu>
2020-10-31 13:19:35 -0500

    Create AddressRepo.java

---
+++ UserManagement/src/main/java/miu/edu/usermanagement/repository/AddressRepo.java
@@ -0,0 +1,9 @@
+package miu.edu.usermanagement.repository;
+
+import miu.edu.usermanagement.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepo extends JpaRepository<Address, Long> {
+}
